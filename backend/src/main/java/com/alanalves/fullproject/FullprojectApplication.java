4a86271147314098f87971cfda5d3bdc7bd33e14
Alan Alves <alan.alveses@gmail.com>
2024-01-03 00:25:59 -0300

    initial commit

---
+++ backend/src/main/java/com/alanalves/fullproject/FullprojectApplication.java
@@ -0,0 +1,13 @@
+package com.alanalves.fullproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullprojectApplication.class, args);
+	}
+
+}
